0024dc39c7b003c4a4554413001f7284088efa9d
Artem <alakomow@ya.ru>
2013-12-21 06:34:00 +0700

    full comlite

--- func.js
+++ func.js
@@ -3,18 +3,10 @@ function Shape(x,y)
 
     return function(func_name,args)
     {
-       if (func_name =="getX") {
-          return x;
-       }else if (func_name=="getY") {
-         return y;
-       }else if (func_name=="setX") {
-         return Shape(args[0],y);
-       }else if (func_name=="setY"){
-         return Shape(x,args[0]);
-       }else if (func_name=="move") {
+       if (func_name=="move") {
          return Shape(x+args[0],y+args[1]);
        }else if (func_name=="print") {
-         return "\nShape x ="+x+"; y="+y+"\n";
+         return "Shape x ="+x+"; y="+y;
        }else{
         return;
        }
@@ -25,22 +17,10 @@ function Rectangle(x,y,w,h)
 {
     return function(func_name,args)
     {
-        if (func_name=="getW") {
-            return w;
-        }else if (func_name=="setW") {
-            return Rectangle(x,y,args[0],h);
-        }else if (func_name=="getH") {
-            return h;
-        }else if (func_name=="setH") {
-            return Rectangle(x,y,w,args[0]);
-        }else if (func_name=="setX") {
-            return Rectangle(args[0],y,w,h);
-        }else if (func_name=="setY") {
-            return Rectangle(x,args[0],w,h);
-        }else if (func_name=="move") {
+       if (func_name=="move") {
             return Rectangle(x+args[0],y+args[1],w,h);
         }else if (func_name=="print") {
-            return "\nRectangle x = "+x+" y="+y+" w="+w+" h="+h+"\n";
+            return "Rectangle x = "+x+" y="+y+" w="+w+" h="+h;
         }else{
             return Shape(x,y)(func_name,args);
         }
@@ -49,15 +29,7 @@ function Rectangle(x,y,w,h)
 function Box(x,y,s)
 {
     return function(func_name,args){
-        if (func_name=="getS") {
-            return s;
-        }else if (func_name=="setS") {
-            return Box(x,y,args[0]);
-        }else if (func_name== "setX") {
-            return Box(args[0],y,s);
-        }else if (func_name=="setY") {
-            return Box(x,args[0],s);
-        }else if (func_name=="move") {
+       if (func_name=="move") {
             return Box(x+args[0],y+args[0],s);
         }else if (func_name=="print") {
             return "Box x="+x+" y="+y+" s="+s;
@@ -71,15 +43,8 @@ function Box(x,y,s)
 function Cyrcle(x,y,r)
 {
     return function(func_name,args){
-        if (func_name=="getR") {
-            return s;
-        }else if (func_name=="setR") {
-            return Cyrcle(x,y,args[0]);
-        }else if (func_name== "setX") {
-            return Cyrcle(args[0],y,r);
-        }else if (func_name=="setY") {
-            return Cyrcle(x,args[0],r);
-        }else if (func_name=="move") {
+       
+        if (func_name=="move") {
             return Cyrcle(x+args[0],y+args[0],r);
         }else if (func_name=="print") {
             return "Cyrcle x="+x+" y="+y+" r="+r;
@@ -90,16 +55,25 @@ function Cyrcle(x,y,r)
 }
 
 
-function Composite(X,Y,shapes)
+function Composite(x,y,shapes)
 {
     return function(func_name,args)
     {
         if (func_name=="add") {
           return  Composite(x,y,shapes.push(args[0]));
         }else if (func_name=="move") {
-            return Composite(x+args[0],y+args[1],shapes.forEach(function(item){ return item(func_name,[args[0],args[1]]); }));
+            return Composite(x+args[0],y+args[1],shapes.map(
+                                        function(item){ 
+                                            return item(func_name,[args[0],args[1]]); 
+                                        }
+                                        )
+                    );
         }else if (func_name=="print") {
-            return "Composite:\n"+shapes.forEach(function(item){item(func_name);});
+            return "Composite: x="+x+" y="+y+"\n"+shapes.map(
+                                                        function(item){
+                                                          return  item(func_name);
+                                                        }
+                                                        ).join("\n");
         };
     }
 }
@@ -112,8 +86,18 @@ function testData(){
 	try {
 		document.write("<pre>");
         w("Start");
-       var shape = Composite(1,2,4);
-        w(shape);
+       var shape = Rectangle(4,4,4,5);
+        
+       var composite = Composite(0,0,[Rectangle(1,1,2,3),Box(2,2,4),Cyrcle(3,3,7)]);
+       composite("add",[shape]);
+       w("Current state: \n"+composite("print"));
+
+       composite = composite("move",[1,1]);
+       w("\n\n");
+       w("Move (1,1)\n"+composite("print"));
+       
+
+       w("\n\nMove (-1,-1)\n"+composite("move",[-1,-1])("print"));
        
 
 	} catch (e) {
